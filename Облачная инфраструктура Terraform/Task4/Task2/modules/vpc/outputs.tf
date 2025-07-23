f690c0aa2e71a073935129f214f422f51f7beab2
Hoodooman <snow36@mail.ru>
2025-07-23 11:28:39 +0000

    updadte main branch

--- Облачная инфраструктура Terraform/Task4/Task2/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet" {
-  description = "Information about the created subnet"
-  value       = yandex_vpc_subnet.public
-}
-
-output "network" {
-  description = "Information about the created network"
-  value       = yandex_vpc_network.this
-}
