3d76653bf801b9bf027a1255c3f0a4de6c66fea9
Hoodooman <snow36@mail.ru>
2025-07-25 18:19:14 +0000

    Task4 Task6

---
+++ Облачная инфраструктура Terraform/Task4/Task6/main.tf
@@ -0,0 +1,18 @@
+# To always have a unique bucket name in this example
+resource "random_string" "unique_id" {
+  length  = 8
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
+}
+
+module "s3" {
+  source = "git::https://github.com/terraform-yc-modules/terraform-yc-s3.git"
+
+  bucket_name = "netology-bucket-${random_string.unique_id.result}"
+  folder_id = var.folder_id
+  versioning = {
+    enabled = true
+  }
+}
