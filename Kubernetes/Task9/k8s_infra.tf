b441b099fdac916575d7bafad5603a11a22f6a8d
Hoodooman <popmatrix007@gmail.com>
2025-10-19 13:19:14 +0300

    Task9_3

--- Kubernetes/Task9/k8s_infra.tf
+++ Kubernetes/Task9/k8s_infra.tf
@@ -1,12 +1,12 @@
-data "template_file" "mastersinit" {
-  template = file("masters-init.yml")
+data "template_file" "cloud-init" {
+  template = file("cloud-init.yml")
   vars = {
     ssh_keys = join("\n", [for key in var.ssh_public_keys: "      - ${key}"])
   }  
 }
 
-data "template_file" "nodesinit" {
-  template = file("nodes-init.yml")
+data "template_file" "jumpbox-init" {
+  template = file("jumpbox-init.yml")
   vars = {
     ssh_keys = join("\n", [for key in var.ssh_public_keys: "      - ${key}"])
   }  
@@ -16,13 +16,50 @@ data "yandex_compute_image" "ubuntu" {
   family = "ubuntu-2204-lts"
 }
 
+resource "yandex_compute_instance" "jumpbox" {
+  # depends_on = [null_resource.offline_script]
+  name  = "jumpbox"
+  # Используем длину списка зон для корректного распределения
+  zone  = "ru-central1-a"  # Без modulo для уникальности зон
+  platform_id       = "standard-v3"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.k8s-subnets[0].id
+    nat       = true # Публичный IP для доступа
+  }
+
+  metadata = {
+    # user-data          = count.index == 0 ? data.template_file.mastersinit.rendered : data.template_file.nodesinit.rendered
+    user-data          = data.template_file.jumpbox-init.rendered
+    serial-port-enable = 1
+  }
+
+  scheduling_policy {
+    preemptible = true # Control-plane не должен быть прерываемым !!!
+  }
+  
+}
+
 resource "yandex_compute_instance" "masters" {
   # depends_on = [null_resource.offline_script]
   count = var.cnt_master
   name  = "master-${count.index}"
   # Используем длину списка зон для корректного распределения
-  # zone  = "ru-central1-${element(local.zones, count.index)}"  # Без modulo для уникальности зон
-  zone  = "ru-central1-${element(local.zones, count.index % length(local.zones))}"
+  zone  = "ru-central1-${element(local.zones, count.index)}"  # Без modulo для уникальности зон
+  # zone  = "ru-central1-${element(local.zones, count.index % length(local.zones))}"
   platform_id       = "standard-v3"
 
   resources {
@@ -44,12 +81,13 @@ resource "yandex_compute_instance" "masters" {
   }
 
   metadata = {
-    user-data          = count.index == 0 ? data.template_file.mastersinit.rendered : data.template_file.nodesinit.rendered
+    # user-data          = count.index == 0 ? data.template_file.mastersinit.rendered : data.template_file.nodesinit.rendered
+    user-data          = data.template_file.cloud-init.rendered
     serial-port-enable = 1
   }
 
   scheduling_policy {
-    preemptible = true # Control-plane не должен быть прерываемым !!!
+    preemptible = false # Control-plane не должен быть прерываемым !!!
   }
   
 }
@@ -85,7 +123,7 @@ resource "yandex_compute_instance" "workers" {
   }
 
   metadata = {
-    user-data          = data.template_file.nodesinit.rendered
+    user-data          = data.template_file.cloud-init.rendered
     serial-port-enable = 1
   }  
 
@@ -94,13 +132,13 @@ resource "yandex_compute_instance" "workers" {
   }
 }
 
-resource "null_resource" "wait_for_control_plane" {
-  depends_on = [yandex_compute_instance.masters[0]]
+resource "null_resource" "wait_for_jumpbox" {
+  depends_on = [yandex_compute_instance.jumpbox]
   connection {
     type        = "ssh" 
     user        = "ubuntu"
     private_key = file(pathexpand("~/.ssh/${var.pvk}"))
-    host        = yandex_compute_instance.masters[0].network_interface.0.nat_ip_address
+    host        = yandex_compute_instance.jumpbox.network_interface.0.nat_ip_address
   }    
 
   provisioner "remote-exec" {
@@ -111,12 +149,12 @@ resource "null_resource" "wait_for_control_plane" {
 }
 
 resource "null_resource" "private_key_ansible" {
-  depends_on = [yandex_compute_instance.masters[0], yandex_compute_instance.workers, null_resource.wait_for_control_plane]
+  depends_on = [yandex_compute_instance.masters, yandex_compute_instance.workers, null_resource.wait_for_jumpbox]
   connection {
     type        = "ssh" 
     user        = "ubuntu"
     private_key = file(pathexpand("~/.ssh/${var.pvk}"))
-    host        = yandex_compute_instance.masters[0].network_interface.0.nat_ip_address
+    host        = yandex_compute_instance.jumpbox.network_interface.0.nat_ip_address
   }
 
   provisioner "file" {
@@ -133,11 +171,22 @@ resource "null_resource" "private_key_ansible" {
     destination = "${var.homedir}/kubespray/inventory/mycluster/hosts.yaml"  # Temporary, writable location
   }
 
+  provisioner "file" {
+    source      = "./ansible/inventory/k8s_cluster.yml"
+    destination = "${var.homedir}/kubespray/inventory/mycluster/group_vars/k8s_cluster.yml"  # Temporary, writable location
+  }  
+
+  provisioner "file" {
+    source      = "./ansible/kubernetes-lb"
+    destination = "${var.homedir}/kubespray/inventory/mycluster"  # Temporary, writable location
+  }  
+
 }
 
 resource "null_resource" "proxy" {
   depends_on = [yandex_compute_instance.masters[0], null_resource.private_key_ansible]
   provisioner "local-exec" {
+    # command = "echo 'skip proxy'"
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/inventory/hosts.yaml --become --become-user=root ansible/setup_proxy.yml"
   }
 }
@@ -150,7 +199,7 @@ resource "null_resource" "kubeadm_config" {
     type        = "ssh" 
     user        = "ubuntu"
     private_key = file(pathexpand("~/.ssh/${var.pvk}"))
-    host        = yandex_compute_instance.masters[0].network_interface.0.nat_ip_address
+    host        = yandex_compute_instance.jumpbox.network_interface.0.nat_ip_address
   }    
     inline = [
       ". ${var.homedir}/kubespray/venv/bin/activate",
--- Kubernetes/Task9/output.tf
+++ Kubernetes/Task9/output.tf
@@ -10,3 +10,39 @@ resource "local_file" "ansible_kubespray" {
   filename = "${path.module}/ansible/inventory/hosts.yaml"
 }
 
+resource "local_file" "proxy_k8s_cluster" {
+   content = templatefile("${path.module}/ansible/templates/k8s_cluster.tftpl",
+     {
+      master_ip = yandex_compute_instance.masters[0].network_interface[0].ip_address
+     }
+   )
+   filename = "${path.module}/ansible/inventory/k8s_cluster.yml"
+}
+
+resource "local_file" "ssh_connect" {
+   content = templatefile("${path.module}/ansible/templates/ssh_connect.tftpl",
+     {
+      masters = yandex_compute_instance.masters[*]
+      jumpbox = yandex_compute_instance.jumpbox
+     }
+   )
+   filename = "${path.module}/ansible/inventory/ssh_connect.ini"
+}
+
+resource "local_file" "control_plane" {
+   content = templatefile("${path.module}/ansible/kubernetes-lb/templates/HA_cp_hosts.tftpl",
+     {
+      masters = yandex_compute_instance.masters[*]
+     }
+   )
+   filename = "${path.module}/ansible/kubernetes-lb/hosts.ini"
+}
+
+resource "local_file" "lg_group_all" {
+   content = templatefile("${path.module}/ansible/kubernetes-lb/templates/all.tftpl",
+     {
+      masters = yandex_compute_instance.masters[*]
+     }
+   )
+   filename = "${path.module}/ansible/kubernetes-lb/group_vars/all.yml"
+}
